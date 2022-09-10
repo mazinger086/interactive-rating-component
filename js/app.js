b446d5482a7a7d89ddf9b697c582c4474ff4aa91
Daniel Lasauskas <Mazinger_086@yahoo.com>
2022-09-10 18:18:19 -0300

    Uploading to Github

---
+++ js/app.js
@@ -0,0 +1,27 @@
+const numbers = document.querySelectorAll('.numbers');
+const submit = document.querySelector('#submit');
+const card = document.querySelector('.card');
+const thanks = document.querySelector('.thanks');
+const choosen = document.querySelector('#choosen');
+
+for (const number of numbers) {
+    number.addEventListener('click', (e) => {
+        let selected = e.target;
+        selected.style = "background-color: hsl(217, 12%, 63%); color: #ffffff;"
+        console.log(e.target.innerText);
+        choosen.innerText = selected.innerText;
+    })
+}
+
+// Event Listeners
+
+submit.addEventListener('click', () => {
+    card.style = "display: none";
+    thanks.style = "display: flex";
+    
+})
+
+
+setTimeout(()=> {
+    location.reload()
+},5000)
